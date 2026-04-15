5aa551059ac7e56a4b5cd85ae13f9ed228fde12b
Mark Ghian Francis <zetsuya6167@gmail.com>
2026-04-15 13:53:06 +0800

    build: set up the tailwind for the initial styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./global.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,15 +8,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full antialiased">
+      <body className="min-h-full flex flex-col font-body">
+        <div className="main-container w-full">{children}</div>
+      </body>
     </html>
   );
 }
